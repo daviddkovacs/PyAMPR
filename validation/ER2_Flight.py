ba2a87b85fc1d1baaae1182386f2299241cd4185
Dávid D.Kovács <david.kovacs@tuwien.ac.at>
2025-10-23 16:42:21 +0200

    Class to read ER-2 data

--- validation/ER2_Flight.py
+++ validation/ER2_Flight.py
@@ -1,12 +1,14 @@
 import os.path
 import rioxarray
+import matplotlib
+matplotlib.use("TkAgg")  # or "Qt5Agg" if you have PyQt5 installed
 import matplotlib.pyplot as plt
 import pyampr
 import xarray as xr
 import pandas as pd
 import os
 
-class ER2_Flight():
+class FlightData:
 
     def __init__(self,
                  path,
@@ -47,25 +49,28 @@ class ER2_Flight():
         comp = {var: {"zlib": True, "complevel": 4} for var in dataset.data_vars}
         dataset.to_netcdf(os.path.join(outpath, fname), encoding=comp)
 
+    def long_plot(self,frequency):
 
-    def save_tiff(self,
-                  dataset,
-                  variable):
-        data_107 = dataset[variable]
-        data_107.rio.write_crs(4326, inplace=True)
-        data_107.rio.set_spatial_dims("Latitude", "Longitude")
-        data_107.rio.to_raster("/home/ddkovacs/Desktop/xxx.tiff")
+        lon = self.to_pandas()["Longitude"]
+        freq_vals = self.to_pandas()[f"MPDI {frequency}"]
+
+        plt.figure(figsize=(8,4))
+        plt.plot(lon, freq_vals, linewidth=1)
+        plt.xlabel("Longitude")
+        plt.ylabel(f"MPDI {frequency}")
+        plt.title(f" Flight: {self.date}\n Direction: {self.flight_direction}\n scan: {self.scan_direction}")
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show(block=True)
+
+ER2_flight = FlightData(path ="/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/WHYMSIE/data_from_RichDJ",
+                      date = "25_Oct",
+                      scan_direction = "26_50",
+                      flight_direction = "WE", )
 
 
 
-MyFlight = ER2_Flight(path ="/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/WHYMSIE/data_from_RichDJ",
-              date = "22_Oct",
-              scan_direction = "1_25",
-              flight_direction = "EW",)
-
-
-
-MyFlight.save_nc("/home/ddkovacs/Desktop/")
+ER2_flight.long_plot("10.7")
 
 
 
